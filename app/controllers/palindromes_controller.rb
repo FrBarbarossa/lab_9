5d7d67c825f69de6db7a06b05505c05db11c247b
FrBarbarossa <makushinsp@mail.ru>
2022-12-18 15:33:16 +0300

    Rubocop fixed. Extra-jobs deleted

--- app/controllers/palindromes_controller.rb
+++ app/controllers/palindromes_controller.rb
@@ -2,57 +2,44 @@
 
 # Controller for /palindromes
 class PalindromesController < ApplicationController
-    before_action :check_num, only: :result
-    after_action :clear_flash, only: :result
-  
-  
-    def index
+  before_action :check_num, only: :result
+  after_action :clear_flash, only: :result
 
-    end
-  
-    def result
-      # redirect_to '/palindromes.js' unless flash.empty?
-      p flash
-      # redirect_to '/'  unless flash.empty?
-      render turbo_stream: turbo_stream.update("flash", partial: "layouts/flash") unless flash.empty?
-      p flash.empty?
-      return unless flash.empty?
-      p '!!!'
-      @result = count_result(params[:number])
-      respond_to do |format|
-        format.html
-        format.turbo_stream 
-        format.json do
-          render json:
-            { type: @result.class.to_s, value: @result }
-        end
-      end
-    end
-  
-    private
+  def index; end
 
-    def clear_flash
-      flash.discard
-    end
-  
-    def check_num
-      number = params[:number]
-      return if number.nil?
-      if Integer(number, exception: false).nil?
-        flash[:notice] = "'#{number}' не является числом"
-        return
-      end
-      if number.to_i <= 0  then
-        flash[:notice] = "Вы ввели: '#{number}' Введите число, больше 0."
-      end
-    end
-  
-    def palindrome?(str)
-      str == str.reverse
-    end
-  
-    def count_result(number)
-      (1..number.to_i).each.select { |num| if palindrome?(num.to_s) && palindrome?((num**2).to_s) then num end }.map { |i| [i, i**2] }
+  def result
+    render turbo_stream: turbo_stream.update('flash', partial: 'layouts/flash') unless flash.empty?
+    return unless flash.empty?
+
+    @result = rslt.map { |i| [i, i**2] }
+    respond_to do |format|
+      format.html
+      format.turbo_stream
     end
   end
-  
+
+  private
+
+  def rslt
+    (1..params[:number].to_i).each.select { |num| num if palindrome?(num.to_s) && palindrome?((num**2).to_s) }
+  end
+
+  def clear_flash
+    flash.discard
+  end
+
+  def check_num
+    number = params[:number]
+    return if number.nil?
+
+    if Integer(number, exception: false).nil?
+      flash[:notice] = "'#{number}' не является числом"
+      return
+    end
+    flash[:notice] = "Вы ввели: '#{number}' Введите число, больше 0." if number.to_i <= 0
+  end
+
+  def palindrome?(str)
+    str == str.reverse
+  end
+end
